31737b66f4fcf668754150eb66c7c5054d09229e
Sebastian Hansen <sebastian.hansen_vega@outlook.com>
2024-05-22 20:44:21 -0700

    updated build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,43 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const basenameProd = '/react-shadcn-starter'
+const basenameProd = '/react-shadcn-starter/';
 
 export default defineConfig(({ command }) => {
-  const isProd = command === 'build'
+  const isProd = command === 'build';
 
   return {
     base: isProd ? basenameProd : '',
-    plugins: [react()],
+    plugins: [
+      react(),
+      commonjs(),
+      nodeResolve({ browser: true }),
+    ],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     define: {
-      global: {
-        basename: isProd ? basenameProd : '',
-      },
+      'process.env': {},
+      global: 'globalThis',
     },
-  }
-})
+    build: {
+      outDir: 'build',
+      rollupOptions: {
+        external: [],
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1000,
+    },
+  };
+});
